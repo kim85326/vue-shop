fe009acfedcf2497c0df9b2f8ee186f42147cc03
elaine <kim85326@gmail.com>
2020-02-25 22:54:08 +0800

    加入 element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
+import ElementUI from "element-ui";
 import App from "./App.vue";
 import router from "./router";
+
 import "reset-css";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
