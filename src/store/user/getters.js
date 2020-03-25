224117a15bfe1cc8f1a8a5d2f5beb19d71f5407e
Elaine_Li <elaine_li@mail.chungyo.net>
2020-03-25 16:42:50 +0800

    實作用戶 getters

---
+++ src/store/user/getters.js
@@ -0,0 +1,18 @@
+export default {
+  // List
+  getListParam(state) {
+    return {
+      page: state.pagination.currentPage,
+      pageSize: state.pagination.pageSize,
+      keyword: state.listQuery.keyword !== undefined ? state.listQuery.keyword : undefined
+    };
+  },
+
+  // Dialog
+  isDialogFormUpdateMode(state) {
+    return state.dialogForm.id !== 0;
+  },
+  isDialogFormPasswordEnabled(state) {
+    return state.dialogForm.password !== undefined;
+  }
+};
--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,7 +1,9 @@
 import { state, mutations } from "./mutations";
+import getters from "./getters";
 
 export default {
   namespaced: true,
   state,
-  mutations
+  mutations,
+  getters
 };
