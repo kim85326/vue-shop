a359b68a3c03a30a6558829ce11ac5458e32608a
Elaine_Li <elaine_li@mail.chungyo.net>
2020-03-25 16:31:33 +0800

    調整 lint 規則，允許同作用域內重複 state 變數名稱

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     quotes: ["error", "double"],
-    semi: ["error", "always"]
+    semi: ["error", "always"],
+    "no-shadow": ["error", { allow: ["state"] }]
   }
 };
