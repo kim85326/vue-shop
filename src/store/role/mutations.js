9bd51df13d9e88e8bd3e87a62630dcd42b09dee3
Elaine_Li <elaine_li@mail.chungyo.net>
2020-04-05 15:09:04 +0800

    實作角色列表 state 與 mutation

--- src/store/role/mutations.js
+++ src/store/role/mutations.js
@@ -1,11 +1,23 @@
 export const state = {
   // Summaries
-  rolesSummaries: []
+  rolesSummaries: [],
+
+  // List
+  isListLoading: false,
+  roles: [],
 };
 
 export const mutations = {
   // Summaries
   setRolesSummaries(state, rolesSummaries) {
     state.rolesSummaries = rolesSummaries;
-  }
+  },
+
+  // List
+  setListLoading(state, isLoading) {
+    state.isListLoading = isLoading;
+  },
+  setRoles(state, roles) {
+    state.roles = roles;
+  },
 };
