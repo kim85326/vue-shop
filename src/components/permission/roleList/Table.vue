150997f6a53dee3965fe2ca85b47cf5797566b79
Elaine_Li <elaine_li@mail.chungyo.net>
2020-04-06 00:12:59 +0800

    實作角色表格元件

---
+++ src/components/permission/roleList/Table.vue
@@ -0,0 +1,73 @@
+<template>
+	<el-table
+		:data="roles"
+		border
+		style="width: 100%"
+		v-loading="isListLoading"
+	>
+		<el-table-column
+			prop="id"
+			label="編號"
+			width="70"
+			align="center"
+		>
+		</el-table-column>
+		<el-table-column
+			prop="name"
+			label="角色名稱"
+			width="250"
+			align="center"
+		>
+		</el-table-column>
+		<el-table-column
+			prop="description"
+			label="描述"
+			min-width="250"
+			align="center"
+		>
+		</el-table-column>
+		<el-table-column
+			prop="createdTime"
+			label="建立時間"
+			width="180"
+			align="center"
+		>
+		</el-table-column>
+		<el-table-column
+			label="操作"
+			width="120"
+			align="center"
+		>
+			<template>
+				<el-button
+					size="mini"
+					type="text"
+				>
+					編輯
+				</el-button>
+				<el-button
+					size="mini"
+					type="text"
+				>
+					刪除
+				</el-button>
+			</template>
+		</el-table-column>
+	</el-table>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+	computed: {
+		...mapState("role", ["roles", "isListLoading"])
+	},
+	methods: {
+		...mapActions("role", ["fetchRoles"])
+	},
+	created() {
+		this.fetchRoles();
+	}
+};
+</script>
---
+++ src/views/permission/RoleList.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<Table />
+	</div>
+</template>
+
+<script>
+import Table from "@/components/permission/roleList/Table.vue";
+
+export default {
+	components: {
+		Table
+	}
+};
+</script>
