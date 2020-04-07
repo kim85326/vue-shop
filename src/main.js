501acc393d6d3504abab0521da768ff4566822a3
Elaine_Li <elaine_li@mail.chungyo.net>
2020-04-07 12:25:04 +0800

    調整重置樣式為 scss

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "reset-css";
 import "element-ui/lib/theme-chalk/index.css";
-import "@/styles/base.css";
 
 Vue.use(ElementUI);
 Vue.use(VeeValidate, { inject: false });
