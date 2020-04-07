4eda2a3b61c4098b6de4a11ff0a89ff1884a899f
Elaine_Li <elaine_li@mail.chungyo.net>
2020-04-07 12:28:00 +0800

    調整使用 scss 方式引入 element UI 樣式

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "element-ui/lib/theme-chalk/index.css";
-
 Vue.use(ElementUI);
 Vue.use(VeeValidate, { inject: false });
 
