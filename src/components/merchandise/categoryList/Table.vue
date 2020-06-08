612360ce15095a5b5de129a261c151be1a6d3c64
Elaine_Li <elaine_li@mail.chungyo.net>
2020-06-08 17:18:08 +0800

    實作刪除分類功能

--- src/components/merchandise/categoryList/Table.vue
+++ src/components/merchandise/categoryList/Table.vue
@@ -28,7 +28,7 @@
 			</template>
 		</el-table-column>
 		<el-table-column
-			v-if="hasUpdatePermission"
+			v-if="hasUpdatePermission || hasDeletePermission"
 			label="操作"
 			width="120"
 			align="center"
@@ -42,18 +42,27 @@
 				>
 					編輯
 				</el-button>
+				<el-button
+					v-if="hasDeletePermission"
+					size="mini"
+					type="text"
+					@click="deleteCategory(scope.row)"
+				>
+					刪除
+				</el-button>
 			</template>
 		</el-table-column>
 	</el-table>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
 	data() {
 		return {
 			hasUpdatePermission: false,
+			hasDeletePermission: false,
 		};
 	},
 
@@ -62,6 +71,10 @@ export default {
 	},
 
 	methods: {
+		...mapActions("category", [
+			"deleteCategory",
+		]),
+
 		handleUpdate(row) {
 			this.$store.commit("category/setDialogVisible", true);
 			this.$store.commit("category/setDialogForm", { ...row });
@@ -71,6 +84,7 @@ export default {
 	async created() {
 		this.$store.dispatch("category/fetchCategories");
 		this.hasUpdatePermission = await this.$store.dispatch("auth/hasPermission", "category-update");
+		this.hasDeletePermission = await this.$store.dispatch("auth/hasPermission", "category-delete");
 	}
 };
 </script>
